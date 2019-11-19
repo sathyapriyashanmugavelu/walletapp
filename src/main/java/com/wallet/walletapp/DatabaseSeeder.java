679d41c2a1a75753a4a1db97a7ecfddc9c11199b
MonikaMahanthappa <monika@mavenhive.in>
2019-11-19 06:02:18 +0530

    Add. Seed users

--- src/main/java/com/wallet/walletapp/DatabaseSeeder.java
+++ src/main/java/com/wallet/walletapp/DatabaseSeeder.java
@@ -1,7 +1,9 @@
 package com.wallet.walletapp;
 
+import com.wallet.walletapp.user.User;
+import com.wallet.walletapp.user.UserRepository;
 import com.wallet.walletapp.wallet.Wallet;
-import com.wallet.walletapp.wallet.WalletRepository;
+import com.wallet.walletapp.wallet.WalletService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +11,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DatabaseSeeder {
     @Bean
-    CommandLineRunner initDatabase(WalletRepository repository) {
+    CommandLineRunner initDatabase(UserRepository repository, WalletService walletService) {
         return args -> {
-            repository.save(new Wallet(100L));
-            repository.save(new Wallet(200L));
+            if (repository.findByUserName("seed-user-1").isEmpty()) {
+                User savedUser = repository.save(new User("seed-user-1", "foobar"));
+                Wallet wallet = new Wallet(0L);
+                wallet.setUser(savedUser);
+                walletService.create(wallet);
+            }
+            if (repository.findByUserName("seed-user-2").isEmpty()) {
+                User savedUser = repository.save(new User("seed-user-2", "foobar"));
+                Wallet wallet = new Wallet(0L);
+                wallet.setUser(savedUser);
+                walletService.create(wallet);
+            }
         };
     }
 }
