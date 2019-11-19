837788e6c0c8b4562a02dce7bd1437dffc2543f9
MonikaMahanthappa <monika@mavenhive.in>
2019-11-19 06:11:15 +0530

    Add. Basic authentication

---
+++ src/main/java/com/wallet/walletapp/user/UserService.java
@@ -0,0 +1,31 @@
+package com.wallet.walletapp.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUserName(),
+                user.getPassword(),
+                AuthorityUtils.createAuthorityList()
+        );
+    }
+
+    public User findUserByUsername(String username) {
+        return userRepository.findByUserName(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+
+}
--- src/test/java/com/wallet/walletapp/wallet/WalletControllerTest.java
+++ src/test/java/com/wallet/walletapp/wallet/WalletControllerTest.java
@@ -1,9 +1,11 @@
 package com.wallet.walletapp.wallet;
 
+import com.wallet.walletapp.user.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
@@ -11,12 +13,16 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(WalletController.class)
+@WithMockUser
 class WalletControllerTest {
     @Autowired
     MockMvc mockMvc;
 
     @MockBean
     WalletService walletService;
+
+    @MockBean
+    UserService userService;
 
     @Test
     void shouldGetWalletById() throws Exception {
