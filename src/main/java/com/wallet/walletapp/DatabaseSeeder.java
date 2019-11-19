115a1a521e2755db1070de457d92dfbaec78fc72
MonikaMahanthappa <monika@mavenhive.in>
2019-11-19 07:05:55 +0530

    Refactor. Move user creation to user service from dataseeder

--- src/main/java/com/wallet/walletapp/DatabaseSeeder.java
+++ src/main/java/com/wallet/walletapp/DatabaseSeeder.java
@@ -2,6 +2,7 @@ package com.wallet.walletapp;
 
 import com.wallet.walletapp.user.User;
 import com.wallet.walletapp.user.UserRepository;
+import com.wallet.walletapp.user.UserService;
 import com.wallet.walletapp.wallet.Wallet;
 import com.wallet.walletapp.wallet.WalletService;
 import org.springframework.boot.CommandLineRunner;
@@ -11,19 +12,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DatabaseSeeder {
     @Bean
-    CommandLineRunner initDatabase(UserRepository repository, WalletService walletService) {
+    CommandLineRunner initDatabase(UserRepository repository, UserService userService) {
         return args -> {
             if (repository.findByUserName("seed-user-1").isEmpty()) {
-                User savedUser = repository.save(new User("seed-user-1", "foobar"));
-                Wallet wallet = new Wallet(0L);
-                wallet.setUser(savedUser);
-                walletService.create(wallet);
+                userService.create(new User("seed-user-1", "foobar"));
+
             }
             if (repository.findByUserName("seed-user-2").isEmpty()) {
-                User savedUser = repository.save(new User("seed-user-2", "foobar"));
-                Wallet wallet = new Wallet(0L);
-                wallet.setUser(savedUser);
-                walletService.create(wallet);
+                userService.create(new User("seed-user-2", "foobar"));
             }
         };
     }
--- src/main/java/com/wallet/walletapp/user/UserService.java
+++ src/main/java/com/wallet/walletapp/user/UserService.java
@@ -1,5 +1,7 @@
 package com.wallet.walletapp.user;
 
+import com.wallet.walletapp.wallet.Wallet;
+import com.wallet.walletapp.wallet.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +13,9 @@ import org.springframework.stereotype.Service;
 public class UserService implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    WalletService walletService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -25,6 +30,14 @@ public class UserService implements UserDetailsService {
 
     public User findUserByUsername(String username) {
         return userRepository.findByUserName(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    public User create(User user) {
+        User savedUser = userRepository.save(user);
+        Wallet wallet = new Wallet(0L);
+        wallet.setUser(savedUser);
+        walletService.create(wallet);
+        return savedUser;
     }
 
 
