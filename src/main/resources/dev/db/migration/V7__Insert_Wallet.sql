3ee4c86d557d15864a0941f1a5dfe36eac2bc0f4
Yasmin <jeelani.yasmin@gmail.com>
2019-11-28 08:17:12 +0530

    1 Wallet per User

--- src/main/resources/dev/db/migration/V7__Insert_Wallet.sql
+++ src/main/resources/dev/db/migration/V7__Insert_Wallet.sql
@@ -3,19 +3,8 @@ VALUES (1,
         100,
         1) ON CONFLICT DO NOTHING;
 
-
-INSERT INTO wallet (id,balance,user_id)
-VALUES (2,
-        100,
-        1) ON CONFLICT DO NOTHING;
-
 INSERT INTO wallet (id,balance,user_id)
 VALUES (3,
         100,
         2) ON CONFLICT DO NOTHING;
 
-
-INSERT INTO wallet (id,balance,user_id)
-VALUES (4,
-        100,
-        2) ON CONFLICT DO NOTHING;
